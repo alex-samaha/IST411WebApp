1d7cbfd30d340d0b82532b9fb5ac5d424ec83c31
Alex S <ags12432@gmail.com>
2018-10-28 22:38:30 -0400

    minor fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-.connect(db)
+.connect(db, {useNewUrlParser: true})
 .then(() => console.log("MongoDB Connection Established"))
 .catch(err => console.log(err));
 
